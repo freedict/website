7b48321952df596a710f0d1dcc6b75c48f04124c
Sebastian Humenda <shumenda@gmx.de>
2018-03-11 23:38:49 +0100

    FreeDict plugin: add localisation support
    The FreeDict plugin now allows for localisation of its messages. This
    assumes the latest patch to th elektor-i18n-plugin to be present.

--- packages/freedict/lektor_freedict.py
+++ packages/freedict/lektor_freedict.py
@@ -1,17 +1,19 @@
+import datetime
 import gettext
 import json
 import os
 import re
 import subprocess
 import sys
+import tempfile
+import time
 import urllib.request
 
-from lektor.pluginsystem import Plugin
 from lektor.context import get_ctx
+from lektor.pluginsystem import Plugin
+from lektor.utils import portable_popen
 
-_ = None
-
-class HTML(object):
+class HTML():
     """A simple wrapper which instructs lektor to not escape HTML tags in the
     resulting file."""
     def __init__(self, html):
@@ -44,15 +46,51 @@ class FreedictPlugin(Plugin):
     name = 'freedict'
     description = 'FreeDict page generator functionality'
 
-    def on_process_template_context(self, context, **extra):
-        def test_function():
-            return 'Value from plugin %s' % self.name
-        context['test_function'] = test_function
+    def get_used_languages(self):
+        """Parse ISO language codes and FreeDict API file and compile a list of
+        all existing (English) language names."""
+        table = load_iso_table()
+        # dictionaries are named like "lg1-lg2", split strng and query English
+        # name
+        dictionaries = (d['name'] for d in load_json_api())
+        # compile list of unique ISO codes
+        codes = set(code for dummy in (keys.split('-') for keys in dictionaries)
+                for code in dummy)
+        return [table[code] for code in codes]
 
+
+    #pylint: disable=unused-argument
     def on_setup_env(self, **extra):
         self.env.jinja_env.globals.update(generate_download_section=
                 generate_download_section)
-
+        # craft temporary POT file with languages
+        now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M')
+        now += '+%s'%(time.tzname[0])
+        pot = [re.sub('\n\\s+', '\n', """msgid ""\nmsgstr ""\n
+                "Project-Id-Version: 0.0.0\\n"
+                "Report-Msgid-Bugs-To: freedict@freelists.org\\n"
+                "POT-Creation-Date: {}s\\n"\n "PO-Revision-Date: YEAR-MO-DA HO:MI+ZONE\\n"
+                "Last-Translator: FULL NAME <EMAIL@ADDRESS>\\n"
+                "Language-Team: FreeDict <freedict@freelists.org>\\n"
+                "Language: LANGUAGE\\n"
+                "MIME-Version: 1.0\\n"\n"Content-Type: text/plain; charset=UTF-8\\n"
+                "Content-Transfer-Encoding: 8bit\\n"\n""".format(now))]
+        for lang in self.get_used_languages():
+            pot.append('\n#: iso-639-3.tab (no:lineno)\n')
+            pot.append('msgid "{}"\nmsgstr ""\n'.format(lang))
+        if not os.path.exists("i18n"):
+            os.mkdir("i18n")
+        lang_pot = tempfile.NamedTemporaryFile(suffix='pot')
+        lang_pot.write(''.join(pot).encode('UTF-8'))
+        lang_pot.flush()
+        plugin_pot = tempfile.NamedTemporaryFile(suffix='pot')
+        xgettext = portable_popen(['xgettext', '-L', 'Python',
+            '--from-code=UTF-8', os.path.relpath(__file__), '-o',
+            plugin_pot.name])
+        xgettext.wait()
+        with open(os.path.join('i18n', 'plugins.pot'), 'wb') as f:
+            portable_popen(['msgcat', "--use-first", lang_pot.name, '-t',
+                    'UTF-8', plugin_pot.name], stdout=f).wait()
 
 
 def load_iso_table():
@@ -61,62 +99,68 @@ def load_iso_table():
             with open('iso-639-3.tab', 'wb') as f:
                 f.write(u.read())
     with open('iso-639-3.tab', encoding='UTF-8') as f:
-        return {line.split('\t')[0]:
-            re.sub(r'\\s+\(.*?\)$', '', line.split('\t')[-2]) for line in
-            f.read().split('\n')}
+        codes = {}
+        strip_paren = lambda x: re.sub(r'(.*?)\s*\(.*\)$', r'\1', x).strip()
+        for line in f.read().split('\n'):
+            codes[line.split('\t')[0]] = strip_paren(line.split('\t')[-2])
+        return codes
 
 def code2name(isotable, name):
     lg1, lg2 = name.split('-')
     return '%s - %s' % (isotable[lg1], isotable[lg2])
 
 
-def mkpage(dictionaries, codes):
-    languages = {l for k in dictionaries for l in k.split('-')}
-    # ToDo: language names are not localised
-    _ = lambda x: x
-    page = ['\n\n<p><label>', _('Pick a language:'), ('\n\n<select onchange="'
-        'showDictionaries(this);">\n<option value="none">'),
-        _('No language selected…</option>')]
+def mk_dropdown(dictionaries, codes, platform):
+    languages = {_(l) for k in dictionaries for l in k.split('-')}
+    page = ['\n\n<p><label>', _('Pick a language:'), '\n\n<select onchange="'
+        'showDictionaries(this, \'%s\');">\n<option value="none">' % platform,
+        _('No language selected…'), '</option>']
     page.append('\n'.join('<option value="%s">%s</option>' % (l, codes[l])
         for l in sorted(languages, key=lambda l: codes[l])))
     page.append('</select></label></p><hr/>\n\n<ul>')
 
-    # ToDo: locale sorting
-    for name, info in sorted(dictionaries.items()):
-        lg1, lg2 = name.split('-')
-        page.append('\n<li class="dict-entry dict-src-%s ' % lg1)
-        page.append('dict-trg-%s" style="display: none">' % lg2)
-        page.append('<a href="%s">%s - %s</a></li>\n' % (info['URL'], _(codes[lg1]),
-            _(codes[lg2])))
+    # sort dictionaries by local name
+    localised_names = {'%s - %s' % \
+             (_(codes[k.split('-')[0]]), _(codes[k.split('-')[1]])):
+             k for k in dictionaries}
+    for name, code in sorted(localised_names.items()):
+        lg1, lg2 = code.split('-')
+        page.append('\n<li class="dict-src-%s ' % lg1)
+        page.append('dict-trg-%s %s" style="display: none">' % (lg2, platform))
+        page.append('<a href="%s">%s</a></li>\n' % \
+                (dictionaries[code]['URL'], _(name)))
     page.append('\n</ul>\n')
     return ''.join(page)
 
 def generate_download_section(target):
-    """Target is either 'mobile' or 'desktop'."""
-    global _
+    """Target can be either 'mobile' or 'desktop'."""
+    if not target in ('mobile', 'desktop'):
+        raise ValueError("Expected either mobile or desktop as target, got " + \
+                repr(target))
     ctx = get_ctx()
     try:
         translator = gettext.translation("contents",
             os.path.join('i18n', '_compiled'), languages=[ctx.locale], fallback = True)
+        translator.install()
     except AttributeError:
         print("No locale found, assuming English.")
         translator = gettext.translation("contents",
             os.path.join('i18n', '_compiled'), languages=['en'], fallback = True)
 
-    gettext.bindtextdomain('freedict', os.path.join('i18n', '_compiled'))
-    _ = translator.gettext
-
     json_api = load_json_api()
     codes = load_iso_table()
     dictionaries = {}
+    platform = ('slob' if target == 'mobile' else 'dictd')
     for dictionary in json_api:
         try:
-            slob = next(r for r in dictionary['releases'] if r['platform'] == 'slob')
+            url = next(r for r in dictionary['releases'] if r['platform'] ==
+                    platform)
         except StopIteration:
-            raise ValueError("Dictionary %s without slob" % dictionary['name'])
+            raise ValueError("Dictionary %s without release for %s" % (
+                dictionary['name'], platform))
         name = dictionary['name']
-        dictionaries[name] = slob
-    return HTML(mkpage(dictionaries, codes))
+        dictionaries[name] = url
+    return HTML(mk_dropdown(dictionaries, codes, target))
 
 if __name__ == '__main__':
     generate_download_section('mobile')
